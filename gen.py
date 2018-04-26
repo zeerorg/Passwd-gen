83ef36b71316ac223804d6e410c6fa2195cf8cc4
zeerorg <r.g.gupta@outlook.com>
2018-04-26 17:57:16 +0530

    Remeberable Password generator

---
+++ gen.py
@@ -0,0 +1,26 @@
+import secrets
+
+def get_word(num_un):
+  num = str(num_un)
+  with open("eff_short_list") as f:
+    for line in f:
+      if num == line.split("\t")[0]:
+        return line.split("\t")[1][:-1]
+
+  with open("eff_list") as f:
+    for line in f:
+      if num == line.split("\t")[0]:
+        return line.split("\t")[1][:-1]
+
+  return None
+
+
+def gen_random_num(digits=4):
+  num = [1 + secrets.randbelow(6) for x in range(digits)]
+  return int(''.join(map(str, num)))
+
+def main():
+  return ' '.join([get_word(gen_random_num()) for x in range(6)])
+
+if __name__ == '__main__':
+  print(main())
